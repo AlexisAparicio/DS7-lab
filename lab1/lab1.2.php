c538fb131d02810235bab5538ca0a9e3d4a33460
Alexis Aparicio <alexiiz360@gmail.com>
2023-09-07 19:12:09 -0500

    updates

---
+++ lab1/lab1.2.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <title> Laboratorio 1.2</title>
+
+    </head>
+    <body>
+        <?php
+        $n1=1;
+        $n2=1;
+        $Suma=$n1+$n2;
+        echo "suma = ".$Suma. "<br>";
+        echo $n1+$n2;
+        ?>
+    </body>
+</html>
