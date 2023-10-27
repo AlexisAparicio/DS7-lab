d9cb70bcec8adeccd7cb4b877de7b6f1f2cec9a9
Alexis Aparicio <alexiiz360@gmail.com>
2023-10-27 12:51:43 -0500

    update

--- lab7/lab7.7.php
+++ lab7/lab7.7.php
@@ -1,14 +1,15 @@
 <?php
-if (array_key_exists('enviar',$__POST)){
-    include_once "class_lib.php";
+if (array_key_exists('enviar',$_POST)){
+    include "class_lib.php";
 
     $diam=$_POST['diam'];
     $altu=$_POST['altu'];
 
-    $cil= new Cilindro($diam,$altu);
+    $cil=new Cilindro($diam,$altu);
 
     $volumen=$cil->calc_volumen();
     $area=$cil->calc_area();
+    
     echo "<br/> El volumen del cilindro es de ".$volumen." metros cubicos";
     echo "<br/> El area del cilindro es de ".$area." metros cubicos";
 }
